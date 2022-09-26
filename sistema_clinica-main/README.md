abc8b93e2371e883e06899ccd7fef37fc8a08691
DiegoJaldin1 <diegojaldin2@gmail.com>
2022-09-26 09:06:48 -0300

    Primeira entrega
    Entrega do primeiro microsserviço

---
+++ sistema_clinica-main/README.md
@@ -0,0 +1,9 @@
+Diego Jaldin 821119998
+Gabriel Lima Ruas 821235160
+Geovanna Holanda da Silva 819115164
+Pedro Henrique Gomes Candido De Lima 821148496
+Rafael Freitas Venosa 82117385
+Luidy Yutá Pereira Monteiro 82119379
+Anna Claudia Barros Silveira 822144028
+
+Implementação do primeiro microsserviço: cadastro (usamos o thunder client)
---
+++ sistema_clinica-main/cadastro/cadastro.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+// Converte  o corpo da requisição para um objeto JSON
+app.use(express.json())
+
+
+//POST localhost:4000/cadastro
+//nome
+//cpf
+//senha
+
+/*
+    {
+        1: {
+            "nome: "Amanda"
+            "cpf": "12345"
+            "senha:" "batatinha123"
+        },
+        2: {
+            "nome: "Paola"
+            "cpf": "85641"
+            "senha:" "paola123"
+        },
+        ...
+    }
+*/
+
+
+const cadastros = {}
+//indica o n° do cadastro
+cadastro = 0
+
+app.post('/cadastro', (requisicao, resposta) => {
+    cadastro++
+    const {nome, cpf, senha} = requisicao.body
+    cadastros[cadastro] = {nome, cpf, senha}
+    resposta.status(201).send(cadastros[cadastro])
+})
+
+app.get('/cadastro', (requisicao, resposta) => {
+    console.log("Executando...")
+    resposta.send(cadastros)
+})
+
+
+app.listen(4000, () => {
+    console.log("Cadastro. Porta 4000")
+})
