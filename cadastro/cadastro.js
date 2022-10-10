9edfe10f95c93164b6b947056ee0ee9459d7a586
DiegoJaldin1 <diegojaldin2@gmail.com>
2022-10-09 21:44:07 -0300

    Atualização 09/10

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,48 +1,64 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const fs = require('fs')
+const path = require('path')
 
 // Converte  o corpo da requisição para um objeto JSON
-app.use(express.json())
+//app.use(express.json()) 
+
+//Adicionando o modulo pug (para ser o processador de modelo (template engine))
+//const pug = require('pug')
+//Definindo o template engine
 
 
-//POST localhost:4000/cadastro
-//nome
-//cpf
-//senha
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use('/public', express.static(path.join(__dirname, '/public')))
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 
-
-/*
-    {
-        1: {
-            "nome: "Amanda"
-            "cpf": "12345"
-            "senha:" "batatinha123"
-        },
-        2: {
-            "nome: "Paola"
-            "cpf": "85641"
-            "senha:" "paola123"
-        },
-        ...
-    }
-*/
-
-
+//"base" onde os cadastros estarão armazenados (até que o servidor reinicie!!)
 const cadastros = {}
 //indica o n° do cadastro
 cadastro = 0
 
 app.post('/cadastro', (requisicao, resposta) => {
+/*  cadastro++
+    const {nome, cpf, senha} = requisicao.body
+    cadastros[cadastro] = {nome, cpf, senha}
+    resposta.status(201).send(cadastros[cadastro]) */
+
+    //resposta.render('login')
+
+
+    
+
     cadastro++
     const {nome, cpf, senha} = requisicao.body
     cadastros[cadastro] = {nome, cpf, senha}
-    resposta.status(201).send(cadastros[cadastro])
+    console.log(cadastros[cadastro])
+   // const nome = requisicao.body.campo_nome
+   // const cpf = requisicao.body.campo_cpf
+    //const senha = requisicao.body.campo_senha
+    //console.log(`${nome} + ${cpf} + ${senha}`)
+    
+
+    resposta.render('cadastro-teste')
+    console.log("Teste Teste Teste")
 })
 
+
+//localhost:4000/cadastro
 app.get('/cadastro', (requisicao, resposta) => {
-    console.log("Executando...")
-    resposta.send(cadastros)
+/*  console.log("Executando...")
+    resposta.send(cadastros) */
+    //console.log(path.join(__dirname, '../'))
+    //console.log(__dirname)
+    resposta.render('cadastro-teste')
 })
 
 
--- login/login.js
+++ login/login.js
@@ -1,44 +1,9 @@
 const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-
-
-var path = require('path')
 const app = express()
 
-
-app.use(session({secret: 'dfxcgvbhjmnkml'}))
-app.use(bodyParser.urlencoded({extended:true}))
-
-
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-app.use("/public", express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, '/views'))
+//converte o corpo da requisição que era puro texto
+//para um objeto JSON
+app.use(express.json())
 
 
 
-app.post('/views', (requisicao, resposta) => {
-    if(requisicao.body.password == password && requisicao.body.cpf == cpf){
-        requisicao.session.cpf = cpf
-        resposta.render('logado')
-
-    } else {
-        resposta.render('index')
-    }
-})
-
-app.get('/views', (requisicao, resposta) => {
-    if(requisicao.session.cpf) {
-        resposta.render('logado')
-        console.log('Meu CPF logado é: ' + requisicao.session.cpf)
-    } else {
-        resposta.render('index')
-    }
-})
-
-
-
-app.listen(5000, () => {
-    console.log("Login. Porta 5000")
-})
