e485f7b5209840ad85e882f7a5aee9e085ceeac8
DiegoJaldin1 <diegojaldin2@gmail.com>
2022-11-21 11:29:48 -0300

    Prontuário versão final
    O sistema tem:
    - Cadastro;
    -Autenticação e login;
    -Prontuário;

--- projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
+++ projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { InicialComponent } from './views/inicial/inicial.component';
 
-const routes: Routes = [];
+import { TelainicialadmComponent } from './views/telasAdm/telainicialadm/telainicialadm.component';
+import { ListagemadmComponent } from './views/telasAdm/listagemadm/listagemadm.component';
+import { ProntuarioadmComponent } from './views/telasAdm/prontuarioadm/prontuarioadm.component';
+import { CadastrofuncadmComponent } from './views/telasAdm/cadastrofuncadm/cadastrofuncadm.component';
+import { CadastromedadmComponent } from './views/telasAdm/cadastromedadm/cadastromedadm.component';
+import { CadastropacadmComponent } from './views/telasAdm/cadastropacadm/cadastropacadm.component';
+
+
+import { TelainicialfuncComponent } from './views/telasFuncionario/telainicialfunc/telainicialfunc.component';
+import { CadastropacfuncComponent } from './views/telasFuncionario/cadastropacfunc/cadastropacfunc.component';
+
+
+import { TelainicialmedComponent } from './views/telasMedico/telainicialmed/telainicialmed.component';
+import { ListagemmedComponent } from './views/telasMedico/listagemmed/listagemmed.component';
+import { ProntuariomedComponent } from './views/telasMedico/prontuariomed/prontuariomed.component';
+
+
+
+const routes: Routes = [
+
+  {path: '', component: InicialComponent},
+  {path: 'login', component: LoginComponent},
+
+  {path: 'telainicialadm', component:TelainicialadmComponent},
+  {path: 'listagemadm', component:ListagemadmComponent},
+  {path: 'prontuarioadm', component:ProntuarioadmComponent},
+  {path: 'cadastrofuncadm', component:CadastrofuncadmComponent},
+  {path: 'cadastromedadm', component:CadastromedadmComponent},
+  {path: 'cadastropacadm', component:CadastropacadmComponent},
+
+  {path: 'telainicialfunc', component:TelainicialfuncComponent},
+  {path: 'cadastropacfunc', component:CadastropacfuncComponent},
+
+  {path: 'telainicialmed', component:TelainicialmedComponent},
+  {path: 'listagemmed', component:ListagemmedComponent},
+  {path: 'prontuariomed', component:ProntuariomedComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projeto_a3_htmlcssfinal/telas/src/app/views/login/login.component.ts
+++ projeto_a3_htmlcssfinal/telas/src/app/views/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+//'src/app/model/medico/medico';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  cpf=''
+  senha=''
+
+ constructor(
+  private HttpClient: HttpClient,
+  private rota: Router
+  ) { }
+  
+ 
+ connectionLogin(){
+    return this.HttpClient.post<{erro:false, mensagem:''}>('http://localhost:8000/logar',
+    {cpf: this.cpf, senha: this.senha})
+    .subscribe(
+      (res) => {
+        if(res.erro){
+          console.log(res.mensagem)
+        }
+        else{
+          if(res.mensagem.search(/Funcionário/)!=-1){
+            this.rota.navigate(['telainicialfunc'])
+          }else if(res.mensagem.search(/Médico/)!=-1){
+            this.rota.navigate(['telainicialmed'])
+          }else if(res.mensagem.search(/Administrador/)!=-1){
+            this.rota.navigate(['telainicialadm']);
+          }
+        }
+      }
+    )
+    
+  }
 
   ngOnInit(): void {
   }
